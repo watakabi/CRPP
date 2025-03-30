cbc756c58a95e7096ab242c9600f2204f6cc034b
watakabi <watakabi617@gmail.com>
2025-03-30 17:21:24 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 fetch('header.html').then(response => response.text()).then(data => {
-  document.getElementByClassName('Header').innerHTML = data;
+  document.getElementById('header').innerHTML = data;
 });
 fetch('footer.html').then(response => response.text()).then(data => {
-  document.getElementByClassName('Footer').innerHTML = data;
+  document.getElementById('footer').innerHTML = data;
 });
