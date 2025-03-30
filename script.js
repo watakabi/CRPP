ecd7af9a2c4cebf1925d7f2190dc4e1ea231eeaa
watakabi <watakabi617@gmail.com>
2025-03-30 16:59:57 +0900

    Update script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+fetch('header.html').then(response => response.text()).then(data => {
+  document.getElementById('header').innerHTML = data;
+});
+fetch('footer.html').then(response => response.text()).then(data => {
+  document.getElementById('footer').innerHTML = data;
+});
