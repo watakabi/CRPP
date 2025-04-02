59b9e6602f7985878f0afdf9e26d6c03c4a6824a
watakabi <watakabi617@gmail.com>
2025-04-02 15:43:52 +0900

    Update and rename headfootindex to headfootindex.js

---
+++ script/headfootindex.js
@@ -0,0 +1,6 @@
+fetch('sites/header.html').then(response => response.text()).then(data => {
+  document.getElementById('header').innerHTML = data;
+});
+fetch('sites/footer.html').then(response => response.text()).then(data => {
+  document.getElementById('footer').innerHTML = data;
+});
