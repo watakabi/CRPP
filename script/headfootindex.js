de560fde5454fb79d493f0a22fcef41faaf6f469
watakabi <watakabi617@gmail.com>
2025-04-02 15:55:22 +0900

    Update headfootindex.js

--- script/headfootindex.js
+++ script/headfootindex.js
@@ -1,6 +1,6 @@
 fetch('sites/headerindex.html').then(response => response.text()).then(data => {
   document.getElementById('header').innerHTML = data;
 });
-fetch('sites/footer.html').then(response => response.text()).then(data => {
+fetch('sites/footerindex.html').then(response => response.text()).then(data => {
   document.getElementById('footer').innerHTML = data;
 });
